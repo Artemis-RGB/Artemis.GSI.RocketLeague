929e941648ad1ac95a82a9e72bfdfb266f026f26
Diogo Trindade <diogotr7@gmail.com>
2022-09-10 23:53:46 +0100

    Removed Artemis plugin

--- src/Artemis.Plugins.Modules.RocketLeague/DataModels/RocketLeagueColor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Artemis.Plugins.Modules.RocketLeague
-{
-    public struct RocketLeagueColor
-    {
-        public byte Red { get; set; }
-        public byte Green { get; set; }
-        public byte Blue { get; set; }
-    }
-}
